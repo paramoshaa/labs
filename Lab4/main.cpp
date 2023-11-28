1aaf5b21b540b8854d01c872cd40d8089fb8431a
paramoshaa <99839635+paramoshaa@users.noreply.github.com>
2023-11-28 22:39:23 +0300

    Update main.cpp

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -3,10 +3,10 @@
 //сообщение, сказанное другим.
 
 
-#include <string>
 #include <iostream>
+#include <string>
 #include <mpi.h>
-#include <cstdlib>
+#include <string>
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
@@ -24,38 +24,32 @@ int main(int argc, char** argv) {
 
             MPI_Send(message.c_str(), message.size() + 1, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
 
-            char messages[11];
-            MPI_Recv(messages, sizeof(messages), MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if (i % 2 == 0) {
+                char messages[256];
+                MPI_Recv(messages, sizeof(messages), MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-            int j = atoi(&messages[8]);
-
-            if (j % 2 == 0) {
-                std::cout << "a first sage said: " << messages << std::endl;
+                std::cout << "a second sage said: " << messages << std::endl;
             }
         }
     }
     else if (rank == 1) {
-        // 2-й мудрец рассуждает
+        // 2-ой мудрец рассуждает
         for (int i = 0; i < 5; ++i) {
-
-            char message[11];
+            char message[256];
             MPI_Recv(message, sizeof(message), MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-            int j = atoi(&message[8]);
-
-            if (j % 2 == 0) {
-                std::cout << "a second sage said: " << message << std::endl;
+            if (i % 2 == 0) {
+                std::cout << "a first sage said: " << message << std::endl;
             }
 
             std::string messages = "message " + std::to_string(i) + "!";
 
-            std::cout << "I am the second sage and my message is " << messages << std::endl;
+            std::cout << "I am the second sage and my message is " << message << std::endl;
 
             MPI_Send(messages.c_str(), messages.size() + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
         }
     }
 
     MPI_Finalize();
-
     return 0;
 }
